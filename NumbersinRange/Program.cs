a6a884fce43a612a6c225bdf5435d8164cbc349c
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-16 20:15:36 +0300

    Initial commit

---
+++ NumbersinRange/Program.cs
@@ -0,0 +1,14 @@
+﻿class NumbersinRange
+{
+    public static void Main(string[] args)
+    {
+        int beginning = int.Parse(Console.ReadLine());
+        int ending = int.Parse(Console.ReadLine());
+       
+        for (int i = beginning; i<=ending; i++)
+        {
+            Console.WriteLine(i);
+        }
+      
+    }
+}
